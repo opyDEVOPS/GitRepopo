f26a75c39ce0cbb13f3a6a2e092f3b8e32558b8d
osureto1 <seunmalissa@gmail.com>
2023-02-07 22:47:20 -0500

    Completed task

--- AddFunctions.java
+++ AddFunctions.java
@@ -1,2 +1,9 @@
 This is My Add Function
 My Manager Work Completed
+1st Day Java Lines Of Code
+2nd Day Java Lines Of Code
+3rd Day Java Lines Of Code
+4th Day Java Lines Of Code
+5th Day Java Lines Of Code
+
+
